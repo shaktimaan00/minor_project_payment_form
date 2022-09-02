15a8113ab245417fbfafe4da8d3a707ec7dc7b3d
Anurag Mishra <97835032+shaktimaan00@users.noreply.github.com>
2022-09-02 10:43:41 +0530

    new update

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
- 
+let radio1 = document.getElementById("radio1");
+let radio2 = document.getElementById("radio2");
+
+
+radio1.addEventListener('click', () => {
+    radio1.style.background = "#000";
+    radio1.style.border = "none";
+
+    radio2.style.background = "transparent";
+    radio2.style.border = "1px solid #bbb";
+});
+
+radio2.addEventListener("click", () => {
+    radio2.style.background = "#000";
+    radio2.style.border = "none";
+
+    radio1.style.background = "transparent";
+    radio1.style.border = "1px solid #bbb";
+});
